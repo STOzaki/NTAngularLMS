STOzaki/NTAngularLMS

commit 634bdd132564b7ab58e87419a9ccda831fe93e5a

Use const rather than var or let for never-reassigned variables.
The linter forbids 'var' entirely, and albeit less fervently still objects to
'let' when the variable is never reassigned.

src/app/service/pager.service.ts
@@ -1,7 +1,7 @@
 export class PagerService {
   getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10) {
     // calculate total pages
-    let totalPages = Math.ceil(totalItems / pageSize);
+    const totalPages = Math.ceil(totalItems / pageSize);
 
     // ensure current page isn't out of range
     if (currentPage < 1) {
@@ -10,7 +10,8 @@ export class PagerService {
       currentPage = totalPages;
     }
 
-    let startPage: number, endPage: number;
+    const startPage: number;
+    const endPage: number;
     if (totalPages <= 10) {
       // less than 10 total pages so show all
       startPage = 1;
@@ -30,11 +31,11 @@ export class PagerService {
     }
 
     // calculate start and end item indexes
-    let startIndex = (currentPage - 1) * pageSize;
-    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
     // create an array of pages to ng-repeat in the pager control
-    let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+    const pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
       i => startPage + i
     );
 

src/app/admin/books/books.component.ts
@@ -130,7 +130,7 @@ export class BooksComponent implements OnInit {
         this.editBook.publisher
       )
       .subscribe(res => {
-        var index = this.books.findIndex(it => it.id === this.editBook.id);
+        const index = this.books.findIndex(it => it.id === this.editBook.id);
         this.books[index].title = this.book.title;
         this.books[index].author = this.book.author;
         this.books[index].publisher = this.book.publisher;

src/app/admin/publishers/publishers.component.ts
@@ -94,7 +94,7 @@ export class PublishersComponent implements OnInit {
         this.editPublisher.phone
       )
       .subscribe(res => {
-        var index = this.publishers.findIndex(
+        const index = this.publishers.findIndex(
           it => it.id === this.editPublisher.id
         );
         this.publishers[index].name = this.editPublisher.name;

commit 4b0541a226a5e7780fbd110eac402052a4fff501

Fix build failure introduced in 634bdd132564b7ab58e87419a9ccda831fe93e5a
Unlike Java, 'const' variables have to be initialized in the same line,
so though these two variables are never _re_assigned we have to use
'let' rather than 'const'.

src/app/service/pager.service.ts
@@ -10,8 +10,8 @@ export class PagerService {
       currentPage = totalPages;
     }
 
-    const startPage: number;
-    const endPage: number;
+    let startPage: number;
+    let endPage: number;
     if (totalPages <= 10) {
       // less than 10 total pages so show all
       startPage = 1;